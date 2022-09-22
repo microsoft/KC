54c2a5c121175573e0b1c2ef121e25066d712756
Zhiwei <33715328+ArleneYuZhiwei@users.noreply.github.com>
2022-09-22 17:07:06 +0800

    Update patch_blink.sh
    To avoid conflicts

--- papers/ReTraCk/scripts/patch/patch_blink.sh
+++ papers/ReTraCk/scripts/patch/patch_blink.sh
@@ -9,19 +9,19 @@ git clone https://github.com/facebookresearch/BLINK.git
 cd BLINK
 git checkout 5fe254dd64d37332347edc73738edcb56096183f
 
-patch -p0 -i ../ReTraCk/retriever/diff/scripts.diff
-patch -p0 -i ../ReTraCk/retriever/diff/blink.diff
-patch -p0 -i ../ReTraCk/retriever/diff/elq.diff
+patch -p0 -i ../ListQA/retriever/diff/scripts.diff
+patch -p0 -i ../ListQA/retriever/diff/blink.diff
+patch -p0 -i ../ListQA/retriever/diff/elq.diff
 
 echo "Copying assets to ReTraCk location"
 
-/bin/cp -rf blink/ ../ReTraCk/retriever/schema_retriever/dense_retriever/
-/bin/cp -rf elq/ ../ReTraCk/retriever/schema_retriever/dense_retriever/
-/bin/cp -rf elq_slurm_scripts/ ../ReTraCk/retriever/schema_retriever/dense_retriever/
-/bin/cp -rf scripts/ ../ReTraCk/retriever/schema_retriever/dense_retriever/
+/bin/cp -rf blink/ ../ListQA/retriever/schema_retriever/dense_retriever/
+/bin/cp -rf elq/ ../ListQA/retriever/schema_retriever/dense_retriever/
+/bin/cp -rf elq_slurm_scripts/ ../ListQA/retriever/schema_retriever/dense_retriever/
+/bin/cp -rf scripts/ ../ListQA/retriever/schema_retriever/dense_retriever/
 
-/bin/cp -rf ../ReTraCk/retriever/diff/get_candidate_emb.py ../ReTraCk/retriever/schema_retriever/dense_retriever/blink/biencoder/get_candidate_emb.py
-/bin/cp -rf ../ReTraCk/retriever/diff/evaluate_relation.py ../ReTraCk//retriever/schema_retriever/dense_retriever/elq/evaluate_relation.py
+/bin/cp -rf ../ListQA/retriever/diff/get_candidate_emb.py ../ListQA/retriever/schema_retriever/dense_retriever/blink/biencoder/get_candidate_emb.py
+/bin/cp -rf ../ListQA/retriever/diff/evaluate_relation.py ../ListQA/retriever/schema_retriever/dense_retriever/elq/evaluate_relation.py
 
 echo "Cleanup..."
 
