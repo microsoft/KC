dbecf3e0ca5acda94b6ec8909191586ce589bb76
Yiheng Shu <yhshu@smail.nju.edu.cn>
2023-02-14 19:09:59 +0800

    Update domain_dict.py

--- papers/TIARA/src/utils/domain_dict.py
+++ papers/TIARA/src/utils/domain_dict.py
@@ -4,5 +4,5 @@
 import json
 import os.path
 
-download_path = os.path.join(os.path.dirname(__file__), '../../datasets/GrailQA/domain_dict.json')
+download_path = os.path.join(os.path.dirname(__file__), '../../dataset/GrailQA/domain_dict.json')
 fb_domain_dict = json.load(open(file=download_path, mode="r"))
